224a1d7a34b80773723e04dc5be873f2fd201148
Tomasz Jarosiewicz <tomasz@jarosiewicz.pl>
2020-09-16 11:01:21 +0200

    Install React Router

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import styles from './Container.scss';
 
 const Container = props => (
-  console.log('props.children', props.children),
   <div className={styles.component}>
     {props.children}
   </div>
